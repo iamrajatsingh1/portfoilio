9e28dc13cf196f9caad4397c0a2fa4b58bd16389
Rajat Singh <iamrajatsingh2@gmail.com>
2021-05-28 17:02:51 +0530

    some changes

--- js/custom.js
+++ js/custom.js
@@ -20,18 +20,21 @@
 
   // SMOOTHSCROLL
   $(function () {
-    $(".nav-link, .custom-btn-link").on("click", function (event) {
-      var $anchor = $(this);
-      $("html, body")
-        .stop()
-        .animate(
-          {
-            scrollTop: $($anchor.attr("href")).offset().top - 49,
-          },
-          1000
-        );
-      event.preventDefault();
-    });
+    $(".nav-link, .custom-btn-link, .button-got-top").on(
+      "click",
+      function (event) {
+        var $anchor = $(this);
+        $("html, body")
+          .stop()
+          .animate(
+            {
+              scrollTop: $($anchor.attr("href")).offset().top - 49,
+            },
+            1000
+          );
+        event.preventDefault();
+      }
+    );
   });
 
   // TOOLTIP
