cf1711bd1ba2e00274c56d8c662ef78403a48aba
Rajat Singh <iamrajatsingh2@gmail.com>
2021-05-30 14:13:17 +0530

    email feature added

--- js/custom.js
+++ js/custom.js
@@ -50,19 +50,42 @@
 
   $("#contact-form-details").submit(function (event) {
     event.preventDefault();
-    // let formData = $(this).serializeArray();
-    $("#submitFormStatus").empty().append("Submitted successfully.");
-    // $.ajax({
-    //   type: "POST",
-    //   url: "",
-    //   data: formData,
-    //   success: (resp) => {
-    //     if (resp.status) {
-    //       // $( "#result" ).empty().append( response );
-    //       alert(resp);
-    //     } else alert("Something went wrong.");
-    //   },
-    //   dataType: "JSON",
-    // });
+    let formData = $(this).serializeArray();
+    let obj = {};
+    formData.forEach((item) => {
+      if (item.name) obj[item.name] = item.value || "";
+    });
+
+    let data = {
+      user_id: "user_okwQj9dnUhQFaApHUSHLW",
+      accessToken: "9eacfd17554dde5a3fd54bf259b15207",
+      service_id: "iamrajatsingh1_u3n6c0isa",
+      template_id: "reply_template_rajat",
+      template_params: {
+        subject: `${obj.name} messaged you from your Portfolio.`,
+        user_name: obj.name,
+        user_email: obj.email,
+        message: `${obj.message || `${obj.name} tried contacting you.`}`,
+        my_name: "Rajat Singh",
+        my_email: "iamrajatsingh1@gmail.com",
+        reply_to: obj.email,
+      },
+    };
+    $("#submitFormStatus").empty().append("Sending...");
+    $.ajax("https://api.emailjs.com/api/v1.0/email/send", {
+      type: "POST",
+      data: JSON.stringify(data),
+      contentType: "application/json",
+    })
+      .done(function () {
+        $("#submitFormStatus").empty().append("Submitted successfully.");
+      })
+      .fail(function (error) {
+        $("#submitFormStatus")
+          .empty()
+          .append(
+            "Could not send the email, please try again or call at Phone number."
+          );
+      });
   });
 })(jQuery);
